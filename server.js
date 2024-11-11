70035632e33404d742f3e7648bbb4aa2fffcf98e
Amit Pandey <amitpandey21599@gmail.com>
2024-11-11 09:40:58 +0530

    Add server.js file

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+let corsOptions = {
+  origin: "http://localhost:8080",
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Enigmatic application." });
+});
+
+require("./app/routes/tutorial.routes.js")(app);
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
